7ce8c0f96946c3ca8ad4834756e91d7b36be97ad
Igor Lovchikov <igor.lovchikov@charlesworth-group.com>
2024-01-18 17:29:34 +0200

    launch sepearate projects

--- ReactCoreUI/src/setupProxy.js
+++ ReactCoreUI/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:7604';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:7045';
 
 const context =  [
   "/weatherforecast",
